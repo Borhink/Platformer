99e73788ab2771bf3734f8db1f34aab3ead21dad
qhonore <quentin.honore@gtd.eu>
2019-09-23 20:29:02 +0200

    rework side detection

---
+++ Assets/Scripts/CollisionSide2D.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class ContactSide2D
+{
+    public bool right {get; private set;}
+    public bool left {get; private set;}
+    public bool top {get; private set;}
+    public bool bottom {get; private set;}
+
+    public ContactSide2D()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+        right = false;
+        left = false;
+        top = false;
+        bottom = false;
+    }
+    
+    public bool[] Calculate(Collider2D collider, ContactPoint2D[] contacts)
+    {
+        bool[] sides = new bool[5];
+        Vector2 max = collider.bounds.max;
+        Vector2 center = collider.bounds.center;
+        Vector2 point = Vector2.zero;
+
+        foreach(ContactPoint2D c in contacts)
+        {
+            point += (c.point - center);
+        }
+
+        float diagonalAngle = Mathf.Atan2(max.y - center.y, max.x - center.x) * 180 / Mathf.PI;
+        float contactAngle = Mathf.Atan2(point.y, point.x) * 180 / Mathf.PI;
+        
+        if (contactAngle < 0)
+            contactAngle = 360 + contactAngle;
+        if (diagonalAngle < 0)
+            diagonalAngle = 360 + diagonalAngle;
+
+        right = (((contactAngle >= 360 - diagonalAngle) && (contactAngle <= 360)) || ((contactAngle <= diagonalAngle) && (contactAngle >= 0)));
+        left = (((contactAngle >= 180 - diagonalAngle) && (contactAngle <= 180)) || ((contactAngle >= 180) && (contactAngle <= 180 + diagonalAngle)));
+        top = (((contactAngle >= diagonalAngle) && (contactAngle <= 90)) || ((contactAngle >= 90) && (contactAngle <= 180 - diagonalAngle)));
+        bottom = (((contactAngle >= 180 + diagonalAngle) && (contactAngle <= 270)) || ((contactAngle >= 270) && (contactAngle <= 360 - diagonalAngle)));
+
+        return sides;
+    }
+
+    public int WallDirection()
+    {
+        if (right && !left)
+            return 1;
+        if (!right && left)
+            return -1;
+        return 0;
+    }
+
+    public int OppositeWallDirection()
+    {
+        return -WallDirection();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,31 +6,15 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
-	public struct RayCastOrigins
-	{
-		public Vector2 topLeft, topRight;
-		public Vector2 bottomLeft, bottomRight;
-	}
 
-	[Header("Raycasting")]
-	[SerializeField] private float _skinWidth = 0.1f;
-	[SerializeField] private int _horizontalRayCount = 5;
-	[SerializeField] private int _verticalRayCount = 5;
-	[SerializeField] private LayerMask _groundMask;
-	private RayCastOrigins _raycastOrigins;
-	private float _horizontalRaySpacing;
-	private float _verticalRaySpacing;
-	private float _rayLength;
 	private BoxCollider2D _boxCol;
 	private Rigidbody2D _rb;
+    private ContactSide2D _contactSide = new ContactSide2D();
 	private bool _isGrounded = false;
-	private Vector2 _lastVelocity = Vector2.zero;
-	private Vector2 _velocityBeforeCollision = Vector2.zero;
 
 
 	[Header("Wall")]
 	[SerializeField] private float _wallBounciness = 5f;
-	private bool _hasShotInAir = false;
 
 
 	[Header("Gun")]
@@ -44,41 +28,28 @@ public class Player : MonoBehaviour
 	{
 		_rb = GetComponent<Rigidbody2D>();
 		_boxCol = GetComponent<BoxCollider2D>();
-		_rayLength = 0.02f + _skinWidth;
-
-		CalculateRaySpacing();
 	}
 
 	void FixedUpdate()
 	{
-		UpdateRaycastOrigins();
-		_isGrounded = GroundCheck();
+		_isGrounded = _contactSide.bottom;
 
 		if (_isGrounded)
 		{
-			_hasShotInAir = false;
-
 			if (_gun && !_isFiring)
 				_gun.Reload();
 		}
 
 		if (!_isGrounded)
 		{
-			float direction = WallCheck();
-			if (_hasShotInAir && direction != 0)
+			int direction = _contactSide.OppositeWallDirection();
+			if (direction != 0)
 			{
-				if (Mathf.Abs(_velocityBeforeCollision.x) > 4f)
-				{
-					Vector2 velocity = new Vector2(direction * _wallBounciness, _rb.velocity.y / 4);
-					_rb.velocity = velocity;
-					_velocityBeforeCollision = Vector2.zero;
-				}
+                _rb.velocity = new Vector2(direction * _wallBounciness, 1f);
 				_gun.Reload();
-				_hasShotInAir = false;
 			}
 		}
-
-		_lastVelocity = _rb.velocity;
+        _contactSide.Reset();
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////
@@ -89,7 +60,6 @@ public class Player : MonoBehaviour
 		 _isFiring = true;
 		 yield return new WaitForSeconds(0.25f);
 		 _isFiring = false;
-		_hasShotInAir = true;
 	}
 
 	public void Fire()
@@ -125,73 +95,12 @@ public class Player : MonoBehaviour
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////
-	////////                               Raycasting                               ////////
+	////////                               Collisions                               ////////
 	////////////////////////////////////////////////////////////////////////////////////////
-	bool GroundCheck()
+
+
+	void OnCollisionStay2D(Collision2D col)
 	{
-		for (int i = 0; i < _verticalRayCount; i++)
-		{
-			Vector2 rayOrigin = _raycastOrigins.bottomLeft + (Vector2.right * _verticalRaySpacing * i);
-			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, _rayLength, _groundMask);
-			Debug.DrawRay(rayOrigin, Vector2.down * _rayLength, Color.red);
-
-			if (hit)
-				return (hit.point.y <= _boxCol.bounds.min.y + _skinWidth);
-		}
-		return false;
-	}
-
-	float WallCheck()
-	{
-		for (int i = 1; i < _horizontalRayCount - 1; i++)
-		{
-			if (_rb.velocity.x <= 0)
-			{
-				Vector2 rayOrigin = _raycastOrigins.bottomLeft + (Vector2.up * _horizontalRaySpacing * i);
-				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.left, _rayLength, _groundMask);
-				Debug.DrawRay(rayOrigin, Vector2.left * _rayLength, Color.red);
-
-				if (hit && hit.point.x <= _boxCol.bounds.min.x + _skinWidth)
-					return 1;
-			}
-			if (_rb.velocity.x >= 0)
-			{
-				Vector2 rayOrigin = _raycastOrigins.bottomRight + (Vector2.up * _verticalRaySpacing * i);
-				RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right, _rayLength, _groundMask);
-				Debug.DrawRay(rayOrigin, Vector2.right * _rayLength, Color.red);
-
-				if (hit && hit.point.x >= _boxCol.bounds.max.x - _skinWidth)
-					return -1;
-			}
-		}
-		return 0;
-	}
-
-	void UpdateRaycastOrigins()
-	{
-		Bounds bounds = _boxCol.bounds;
-		bounds.Expand(_skinWidth * -2);
-
-		_raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-		_raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-		_raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-		_raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-	}
-
-	void CalculateRaySpacing()
-	{
-		Bounds bounds = _boxCol.bounds;
-		bounds.Expand(_skinWidth * -2);
-
-		_horizontalRayCount = Mathf.Clamp(_horizontalRayCount, 2, int.MaxValue);
-		_verticalRayCount = Mathf.Clamp(_verticalRayCount, 2, int.MaxValue);
-
-		_horizontalRaySpacing = bounds.size.y / (_horizontalRayCount - 1);
-		_verticalRaySpacing = bounds.size.x / (_verticalRayCount - 1);
-	}
-
-	void OnCollisionEnter2D(Collision2D col)
-	{
-		_velocityBeforeCollision = _lastVelocity;
+        _contactSide.Calculate(_boxCol, col.contacts);
 	}
 }
